a941a93b1eed320dd9002cd3c7b818b8f2cf6f8d
Adam Aldous <aldousadam@gmail.com>
2016-02-01 22:21:38 -0700

    First Try

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -12,14 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.blackColor()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    var isOn = true
+    
+    @IBAction func buttonTapped(sender: AnyObject) {
+        if isOn {
+            self.view.backgroundColor = UIColor.whiteColor()
+            isOn = false
+        } else {
+            self.view.backgroundColor = UIColor.blackColor()
+            isOn = true
+        }
+    }
 }
-
